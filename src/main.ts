1e98f12d99dcbe44ae1eab08801e428677ef3ac8
elena-lucky <elena.ivanovada@gmail.com>
2024-05-03 00:17:18 +0300

    fix: fix configs

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import AppController from './components/controller/AppController';
-import './main.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const appController = new AppController();
-  appController.checkLocalStorageAndLoadPage();
-});
