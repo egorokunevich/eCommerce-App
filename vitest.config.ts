e26ed7aab7a24721cf3baa90e213983c04950049
elena-lucky <elena.ivanovada@gmail.com>
2024-05-04 11:27:38 +0300

    chore: RSS-ECOMM-1_16_create_testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default mergeConfig(viteConfig, defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     name: 'happy-dom',
     environment: 'happy-dom',
@@ -10,4 +12,4 @@ export default mergeConfig(viteConfig, defineConfig({
     globals: true,
     restoreMocks: true,
   },
-}))
+}));
