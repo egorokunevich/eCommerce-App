7848dad193ad815d0043d779f57ef4ddd8cc4e6f
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2024-05-09 00:19:25 +0300

    feat: RSS ECOMM 2 01 implement login validation (#64)
    * feat: add login page layout
    
    * feat: implement error messages about client-side user validation
    
    * feat: implement show password functionality
    
    * refactor: lint code
    
    * refactor: move initialization out of constructor
    
    * refactor: clear main.ts
    
    * fix: revive label icons functionality
    
    * refactor: clear main.ts

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,3 +1,163 @@
-export default class LoginPage {
-  // Implement login page with validation and authentication
+import { button, div, form, h2, input, label, p } from '@control.ts/min';
+
+import type { PasswordValidationMessages } from '@/utils/InputValidations';
+import { validateEmailClientSide, validateForm, validatePasswordClientSide } from '@/utils/InputValidations';
+import validationStyles from '@/utils/InputValidations.module.scss';
+
+import styles from './LoginPage.module.scss';
+
+export class LoginPage {
+  private pageWrapper: HTMLElement;
+  private parent: HTMLElement = document.body;
+  private emailInputElement: HTMLInputElement = input({});
+  private passwordInputElement: HTMLInputElement = input({});
+  private emailValidationMessage: HTMLElement = div({});
+  private passwordValidationMsgs: PasswordValidationMessages = {
+    lengthMsg: div({}),
+    uppercaseMsg: div({}),
+    digitMsg: div({}),
+    whitespaceMsg: div({}),
+  };
+  private emailLabel: HTMLElement = div({});
+  private passwordLabel: HTMLElement = div({});
+  private loginBtn: HTMLButtonElement = button({});
+  constructor() {
+    this.pageWrapper = div({ className: `${styles.loginPageWrapper}` });
+  }
+
+  private createEmailInputField(): HTMLElement {
+    const emailContainer = div({ className: `${styles.inputContainer}` });
+
+    const emailInput = input({
+      placeholder: 'Email',
+      className: `${styles.inputField}`,
+      required: true,
+    });
+
+    this.emailInputElement = emailInput;
+
+    this.emailValidationMessage = div({
+      className: `${validationStyles.loginFormErrorMsg}`,
+      txt: `> Wrong email format (user@example.com)`,
+    });
+
+    const emailLabel = label({ className: `${validationStyles.loginFormInputLabel}` });
+    this.emailLabel = emailLabel;
+
+    emailContainer.append(emailLabel, emailInput, this.emailValidationMessage);
+
+    return emailContainer;
+  }
+
+  private createValidationMessage(): HTMLElement {
+    return div({
+      className: `${validationStyles.loginFormErrorMsg}`,
+    });
+  }
+
+  private createPasswordInputField(): HTMLElement {
+    const passwordContainer = div({ className: `${styles.inputContainer}` });
+    const passwordInput = input({
+      type: 'password',
+      placeholder: 'Password',
+      className: `${styles.inputField}`,
+      required: true,
+      autocomplete: 'off',
+      minLength: 8,
+    });
+    this.passwordInputElement = passwordInput;
+    const passwordLengthValidationMessage = this.createValidationMessage();
+    const passwordUppercaseValidationMessage = this.createValidationMessage();
+    const passwordDigitValidationMessage = this.createValidationMessage();
+    const passwordWhitespaceValidationMessage = this.createValidationMessage();
+    this.passwordValidationMsgs = {
+      lengthMsg: passwordLengthValidationMessage,
+      uppercaseMsg: passwordUppercaseValidationMessage,
+      digitMsg: passwordDigitValidationMessage,
+      whitespaceMsg: passwordWhitespaceValidationMessage,
+    };
+    const showPassBtn = div({ className: `${validationStyles.showPasswordBtn}` });
+    showPassBtn.addEventListener('click', () => {
+      passwordInput.type =
+        passwordInput.type === 'password' ? (passwordInput.type = 'text') : (passwordInput.type = 'password');
+    });
+    const passwordLabel = label({ className: `${validationStyles.loginFormInputLabel}` }, showPassBtn);
+    this.passwordLabel = passwordLabel;
+    passwordContainer.append(
+      passwordLabel,
+      passwordInput,
+      passwordLengthValidationMessage,
+      passwordUppercaseValidationMessage,
+      passwordDigitValidationMessage,
+      passwordWhitespaceValidationMessage,
+    );
+    return passwordContainer;
+  }
+
+  public render(): void {
+    const formContainer = div({ className: `${styles.loginContainer}` });
+    const infoContainer = div({ className: `${styles.loginContainerInfo}` });
+    const header = h2({ className: `${styles.infoHeader}`, txt: 'WELCOME' });
+    const info = p({
+      className: `${styles.infoDescription}`,
+      txt: `Nice to see you! Please login via Email and Password.`,
+    });
+    const loginForm = form({ className: `${styles.loginContainerLoginForm}` });
+    const submitContainer = div({
+      className: `${styles.submitContainer}`,
+    });
+    const loginBtn = button({
+      type: 'submit',
+      txt: 'Login',
+      className: `${styles.submitBtn}`,
+      disabled: true,
+    });
+    this.loginBtn = loginBtn;
+    const signUpBtn = button({
+      type: 'button',
+      txt: 'Sign Up',
+      className: `${styles.submitBtn}`,
+      disabled: false,
+    });
+    const status = div({
+      txt: '',
+      className: `${styles.loginStatus}`,
+    });
+    infoContainer.append(header, info);
+    submitContainer.append(loginBtn, signUpBtn);
+    loginForm.append(this.createEmailInputField(), this.createPasswordInputField(), submitContainer, status);
+    formContainer.append(infoContainer, loginForm);
+    this.pageWrapper.append(formContainer);
+    this.parent.append(this.pageWrapper);
+    this.validate(this.emailInputElement, this.passwordInputElement, loginBtn);
+  }
+
+  private validate(emailInput: HTMLInputElement, passwordInput: HTMLInputElement, loginBtn: HTMLButtonElement): void {
+    // Make sure that input values valid. Otherwise disable login button. Check errors
+    const areInputsValid = {
+      email: false,
+      password: false,
+    };
+
+    emailInput.addEventListener('input', () => {
+      emailInput.value = emailInput.value.trim();
+      areInputsValid.email = validateEmailClientSide(emailInput, this.emailValidationMessage, this.emailLabel);
+      validateForm(Object.values(areInputsValid), loginBtn);
+    });
+    passwordInput.addEventListener('input', () => {
+      areInputsValid.password = validatePasswordClientSide(
+        passwordInput,
+        this.passwordValidationMsgs,
+        this.passwordLabel,
+      );
+      validateForm(Object.values(areInputsValid), loginBtn);
+    });
+    this.loginBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  }
+
+  public destroy(): void {
+    this.pageWrapper.remove();
+  }
 }
---
+++ src/utils/InputValidations.ts
@@ -0,0 +1,164 @@
+import styles from '@/utils/InputValidations.module.scss';
+
+export type PasswordValidationMessages = {
+  lengthMsg: HTMLElement;
+  uppercaseMsg: HTMLElement;
+  digitMsg: HTMLElement;
+  whitespaceMsg: HTMLElement;
+};
+
+type ErrorStatusesType = {
+  lengthError: boolean;
+  uppercaseError: boolean;
+  digitError: boolean;
+  whitespaceError: boolean;
+};
+
+function checkLength(
+  passwordInput: HTMLInputElement,
+  inputErrorMsgs: PasswordValidationMessages,
+  errorStatuses: ErrorStatusesType,
+): void {
+  // Check for correct length
+  if (passwordInput.value.length < 8 && passwordInput.value[0]) {
+    inputErrorMsgs.lengthMsg.classList.add(`${styles.errorMsgActive}`);
+    errorStatuses.lengthError = true;
+  } else if (passwordInput.value.length >= 8) {
+    inputErrorMsgs.lengthMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.lengthError = false;
+  } else {
+    inputErrorMsgs.lengthMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.lengthError = false;
+  }
+}
+function checkWhitespaces(
+  passwordInput: HTMLInputElement,
+  inputErrorMsgs: PasswordValidationMessages,
+  errorStatuses: ErrorStatusesType,
+): void {
+  // Check for whiteSpaces
+  const whitespaceRegExp = /\s/;
+
+  if (passwordInput.value[0] && whitespaceRegExp.test(passwordInput.value)) {
+    inputErrorMsgs.whitespaceMsg.classList.add(`${styles.errorMsgActive}`);
+    errorStatuses.whitespaceError = true;
+  } else if (passwordInput.value[0]) {
+    inputErrorMsgs.whitespaceMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.whitespaceError = false;
+  } else {
+    inputErrorMsgs.whitespaceMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.whitespaceError = false;
+  }
+}
+function checkDigits(
+  passwordInput: HTMLInputElement,
+  inputErrorMsgs: PasswordValidationMessages,
+  errorStatuses: ErrorStatusesType,
+): void {
+  const digitRegExp = /\d/;
+
+  // Check for at least 1 digit
+  if (passwordInput.value[0] && digitRegExp.test(passwordInput.value)) {
+    inputErrorMsgs.digitMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.digitError = false;
+  } else if (passwordInput.value[0]) {
+    inputErrorMsgs.digitMsg.classList.add(`${styles.errorMsgActive}`);
+    errorStatuses.digitError = true;
+  } else {
+    inputErrorMsgs.digitMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.digitError = false;
+  }
+}
+function checkUppercase(
+  passwordInput: HTMLInputElement,
+  inputErrorMsgs: PasswordValidationMessages,
+  errorStatuses: ErrorStatusesType,
+): void {
+  const uppercaseRegExp = /(?=.*[a-z])(?=.*[A-Z])/;
+
+  // Check for english uppercase and lowercase
+  if (passwordInput.value[0] && uppercaseRegExp.test(passwordInput.value)) {
+    inputErrorMsgs.uppercaseMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.uppercaseError = false;
+  } else if (passwordInput.value[0]) {
+    inputErrorMsgs.uppercaseMsg.classList.add(`${styles.errorMsgActive}`);
+    errorStatuses.uppercaseError = true;
+  } else {
+    inputErrorMsgs.uppercaseMsg.classList.remove(`${styles.errorMsgActive}`);
+    errorStatuses.uppercaseError = false;
+  }
+}
+
+export function validatePasswordClientSide(
+  passwordInput: HTMLInputElement,
+  inputErrorMsgs: PasswordValidationMessages,
+  inputLabel: HTMLElement,
+): boolean {
+  const errorStatuses = {
+    lengthError: false,
+    uppercaseError: false,
+    digitError: false,
+    whitespaceError: false,
+  };
+
+  inputErrorMsgs.lengthMsg.innerText = `> Should be at least ${passwordInput.minLength} letters`;
+  inputErrorMsgs.uppercaseMsg.innerText = `> Use english uppercase and lowercase`;
+  inputErrorMsgs.digitMsg.innerText = `> Should be at least 1 digit`;
+  inputErrorMsgs.whitespaceMsg.innerText = `> No whitespace allowed`;
+
+  checkLength(passwordInput, inputErrorMsgs, errorStatuses);
+  checkUppercase(passwordInput, inputErrorMsgs, errorStatuses);
+  checkDigits(passwordInput, inputErrorMsgs, errorStatuses);
+  checkWhitespaces(passwordInput, inputErrorMsgs, errorStatuses);
+
+  const isValid = Object.values(errorStatuses).every((status) => status === false);
+
+  if (isValid) {
+    inputLabel.classList.remove(`${styles.inputWarningIcon}`);
+    inputLabel.classList.add(`${styles.inputAcceptIcon}`);
+  } else {
+    inputLabel.classList.add(`${styles.inputWarningIcon}`);
+    inputLabel.classList.remove(`${styles.inputAcceptIcon}`);
+  }
+
+  if (passwordInput.value.length < 1) {
+    inputLabel.classList.remove(`${styles.inputWarningIcon}`);
+    inputLabel.classList.remove(`${styles.inputAcceptIcon}`);
+  }
+
+  return isValid;
+}
+
+export function validateEmailClientSide(
+  emailInput: HTMLInputElement,
+  inputErrorMsg: HTMLElement,
+  inputLabel: HTMLElement,
+): boolean {
+  const regExp = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+
+  if (emailInput.value[0] && regExp.test(emailInput.value)) {
+    inputErrorMsg.classList.remove(`${styles.errorMsgActive}`);
+    inputLabel.classList.remove(`${styles.inputWarningIcon}`);
+    inputLabel.classList.add(`${styles.inputAcceptIcon}`);
+    return true;
+  }
+  if (emailInput.value[0]) {
+    inputErrorMsg.classList.add(`${styles.errorMsgActive}`);
+    inputLabel.classList.add(`${styles.inputWarningIcon}`);
+    inputLabel.classList.remove(`${styles.inputAcceptIcon}`);
+    return false;
+  }
+  inputErrorMsg.classList.remove(`${styles.errorMsgActive}`);
+  inputLabel.classList.remove(`${styles.inputWarningIcon}`);
+  inputLabel.classList.remove(`${styles.inputAcceptIcon}`);
+  return false;
+}
+
+export function validateForm(statuses: boolean[], button: HTMLButtonElement): void {
+  const isValid = statuses.every((status) => status === true);
+  if (isValid) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+}
