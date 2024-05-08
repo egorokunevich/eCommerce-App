d8c8c2817d5ea28eeb1249d528d3f5698ac58432
Glib Shemenkov <shemenkovglib@gmail.com>
2024-05-09 01:37:13 +0200

    feat: 2-09 add registration base style

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 // import './components/css/normalise.css';
 
 import { LoginPage } from '@pages/LoginPage';
+import { pageRegistration } from '@pages/RegistrationPage';
 
 const loginPage = new LoginPage();
 
 loginPage.render();
+
+// const main = document.querySelector('._login-container_mg680_15') as HTMLElement;
+//we must disaded we we will keep
+// const main = document.querySelector('._login-page-wrapper_mg680_1') as HTMLElement;
+// main.append(pageRegistration.createRegistration());
+pageRegistration.createRegistration();
--- src/pages/RegistrationPage.ts
+++ src/pages/RegistrationPage.ts
@@ -1,27 +1,31 @@
 import { button, div, form, h2, input, label, p, section } from '@control.ts/min';
 
 import { setAttributes } from '@/utils/BaseComponentProps';
+import { validateEmailClientSide, validateForm, validatePasswordClientSide } from '@/utils/InputValidations';
+
+import styles from './LoginPage.module.scss';
 
 export default class RegistrationPage {
-  public createRegistration(): void {
+  public createRegistration(): HTMLElement {
     const regSection = section(
       { className: 'registration' },
       div(
-        { className: 'registration_container' },
+        { className: 'styles.loginContainer' },
         div(
-          { className: 'registration_container-form' },
-          h2({ className: 'registration_title', txt: 'REGISTRATION' }),
+          { className: styles.loginContainer },
+          h2({ className: styles.infoHeader, txt: 'REGISTRATION' }),
           this.createFormComponents(),
         ),
         this.createTextLoginComponents(),
       ),
     );
-    document.body.append(regSection);
+    document.querySelector(`.${styles.loginPageWrapper}`)?.append(regSection);
+    return regSection;
   }
 
   private createFormComponents(): HTMLElement {
     const node = form(
-      { className: ['registration_form', 'form'].join(' ') },
+      { className: ['registration_form', 'form', styles.loginContainerLoginForm].join(' ') },
       this.createFormLabel('mail'),
       this.createFormLabel('password'),
       this.createFormLabel('name'),
@@ -36,31 +40,46 @@ export default class RegistrationPage {
   private createFormAddress(): HTMLElement {
     const node = div(
       { className: 'form_address' },
+      p({ txt: 'Address ' }),
       this.createFormLabel('street'),
       this.createFormLabel('city'),
       this.createFormLabel('code'),
       this.createFormLabel('country'),
+      button({ className: styles.submitBtn, txt: 'Register' }),
     );
     return node;
   }
 
   private createFormLabel(text: string): HTMLLabelElement {
-    return label(
-      { txt: `${text.slice(0, 1).toLocaleUpperCase() + text.slice(1)}` },
-      setAttributes(input({ className: 'form_input', id: `user-${text}` }), {
+    //at the end we must made this node Input as HTMLInputElement
+    const inputTag = setAttributes(input({ className: styles.inputField, id: `user-${text}` }), [
+      {
         type: 'user',
         text: `user-${text}`,
-      }),
+      },
+      { type: 'placeholder', text: `${text.slice(0, 1).toLocaleUpperCase() + text.slice(1)}` },
+    ]) as HTMLInputElement;
+    // const inputTag = document.createElement('input');
+    inputTag.addEventListener('input', function () {
+      const inputTag = this.value;
+      //we must start today Egor!!!!!!!
+      // emailValidationMessage(inputTag,)
+    });
+
+    return label(
+      {},
+      inputTag,
+      // { txt: `${text.slice(0, 1).toLocaleUpperCase() + text.slice(1)}` },
     );
   }
   private createTextLoginComponents(): HTMLElement {
     const node = div(
-      { className: ['registration_container-login', 'login'].join(' ') },
-      h2({ className: 'login_title', txt: 'LOGIN' }),
+      { className: ['registration_container-login', 'login', styles.loginContainer].join(' ') },
+      h2({ className: styles.infoHeader, txt: 'LOGIN' }),
       p({
         txt: `Registering on this site will allow you to access the status and history of your orders. Simply fill out the fields below and we'll quickly create a new account for you. We will only ask you the information necessary to make the purchasing process faster and easier`,
       }),
-      button({ className: ['login-btn', 'btn'].join(' '), txt: 'Registering' }),
+      button({ className: ['login-btn', 'btn', styles.submitBtn].join(' '), txt: 'Login' }),
     );
     return node;
   }
