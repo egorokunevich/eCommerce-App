aeebc6ca9f86b321a4050b60d240acaec8d5ffb7
elena-lucky <elena.ivanovada@gmail.com>
2024-05-13 00:51:07 +0300

    feat: RSS-ECOMM-2_22 implement navigation logo

--- src/pages/Layout.ts
+++ src/pages/Layout.ts
@@ -1,18 +1,50 @@
-import type NavMain from '@components/NavMain';
-import { createNavMain } from '@components/NavMain';
+import { div } from '@control.ts/min';
+
+import Header from '@components/Header';
+import NavMain from '@components/NavMain';
+
+import styles from './Layout.module.scss';
 
 export class Layout {
+  private layoutElement: HTMLElement | null = null;
+  private header: Header;
   private navMain: NavMain;
 
   constructor() {
-    this.navMain = createNavMain();
+    this.header = new Header();
+    this.navMain = new NavMain();
   }
 
-  public getNavMain(): NavMain {
-    return this.navMain;
+  private createLayout(): void {
+    const layout = div({ className: styles.headerWrapper });
+    const headerElement = this.header.getHeaderElement();
+    if (headerElement) {
+      layout.appendChild(headerElement);
+    }
+
+    const menuElement = this.navMain.getMenuElement();
+    if (menuElement) {
+      layout.appendChild(menuElement);
+    }
+    this.layoutElement = layout;
+  }
+
+  public getLayoutElement(): HTMLElement | null {
+    if (!this.layoutElement) {
+      this.createLayout();
+    }
+    return this.layoutElement;
+  }
+
+  public renderLayout(): void {
+    const layout = this.getLayoutElement();
+    if (layout) {
+      document.body.appendChild(layout);
+    }
   }
 }
 
-export const createLayout = (): Layout => {
-  return new Layout();
+export const renderLayout = (): void => {
+  const layout = new Layout();
+  layout.renderLayout();
 };
