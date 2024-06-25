3db4e885e9a2ff84294da5b17c66c35c9dd21dfb
egorokuevich <mrcraigre@gmail.com>
2024-06-25 12:04:56 +0300

    feat: display user data and add ability to modify it

--- src/pages/ProfilePage/ProfilePage.ts
+++ src/pages/ProfilePage/ProfilePage.ts
@@ -1,9 +1,11 @@
-import { div, h2, section } from '@control.ts/min';
+import type { Customer } from '@commercetools/platform-sdk';
+import { div, h2, input, section } from '@control.ts/min';
 import { Router } from 'vanilla-routing';
 
+import { showToastMessage } from '@components/Toast';
 import profileService from '@services/ProfileService';
 
-import styles from './UserProfile.module.scss';
+import styles from './ProfilePage.module.scss';
 
 export class ProfilePage {
   private pageWrapper: HTMLElement = section({ className: styles.wrapper });
@@ -23,9 +25,154 @@ export class ProfilePage {
       oldProfile.addEventListener('click', () => {
         Router.go('/profile', { addToHistory: true });
       });
-      container.append(title, oldProfile);
+      const info = this.createUserInfo(userData.body);
+      container.append(title, oldProfile, info);
     }
     this.pageWrapper.append(container);
     return container;
+  }
+
+  private createUserInfo(data: Customer): HTMLElement {
+    const container = div({ className: styles.infoContainer });
+
+    const firstName = this.createFirstNameItem(`${data.firstName}`);
+    const lastName = this.createLastNameItem(`${data.lastName}`);
+    const email = this.createEmailItem(`${data.email}`);
+    const birthday = this.createDateOfBirthItem(`${data.dateOfBirth}`);
+
+    container.append(firstName, lastName, email, birthday);
+    return container;
+  }
+
+  private createFirstNameItem(value: string): HTMLElement {
+    const container = div({ className: styles.itemContainer });
+    const title = div({ className: styles.itemTitle, txt: `First name` });
+    const data = input({ className: styles.itemData, value, disabled: true });
+    const editBtn = div({ className: styles.editBtn });
+    editBtn.addEventListener('click', async () => {
+      this.updateFirstName(data);
+    });
+    data.addEventListener('keydown', async (e) => {
+      if (e.key === 'Enter') {
+        this.updateFirstName(data);
+      }
+    });
+    container.append(title, data, editBtn);
+    return container;
+  }
+
+  private createLastNameItem(value: string): HTMLElement {
+    const container = div({ className: styles.itemContainer });
+    const title = div({ className: styles.itemTitle, txt: `Last name` });
+    const data = input({ className: styles.itemData, value, disabled: true });
+    const editBtn = div({ className: styles.editBtn });
+    editBtn.addEventListener('click', async () => {
+      this.updateLastName(data);
+    });
+    data.addEventListener('keydown', async (e) => {
+      if (e.key === 'Enter') {
+        this.updateLastName(data);
+      }
+    });
+    container.append(title, data, editBtn);
+    return container;
+  }
+
+  private createEmailItem(value: string): HTMLElement {
+    const container = div({ className: styles.itemContainer });
+    const title = div({ className: styles.itemTitle, txt: `Email` });
+    const data = input({ className: styles.itemData, value, disabled: true });
+    const editBtn = div({ className: styles.editBtn });
+    editBtn.addEventListener('click', async () => {
+      this.updateEmail(data);
+    });
+    data.addEventListener('keydown', async (e) => {
+      if (e.key === 'Enter') {
+        this.updateEmail(data);
+      }
+    });
+    container.append(title, data, editBtn);
+    return container;
+  }
+
+  private createDateOfBirthItem(value: string): HTMLElement {
+    const container = div({ className: styles.itemContainer });
+    const title = div({ className: styles.itemTitle, txt: `Birthday` });
+    const data = input({ className: styles.itemData, type: 'date', value, disabled: true });
+    const editBtn = div({ className: styles.editBtn });
+    editBtn.addEventListener('click', async () => {
+      this.updateDateOfBirth(data);
+    });
+    data.addEventListener('keydown', async (e) => {
+      if (e.key === 'Enter') {
+        this.updateDateOfBirth(data);
+      }
+    });
+    container.append(title, data, editBtn);
+    return container;
+  }
+
+  private async updateFirstName(field: HTMLInputElement): Promise<void> {
+    if (field.disabled) {
+      field.disabled = false;
+      field.focus();
+    } else {
+      try {
+        const response = await profileService.updateFirstName(field.value);
+        if (response.statusCode === 200) {
+          field.disabled = true;
+        }
+      } catch (e) {
+        showToastMessage('Failed to save the data. Please, try again.');
+      }
+    }
+  }
+
+  private async updateLastName(field: HTMLInputElement): Promise<void> {
+    if (field.disabled) {
+      field.disabled = false;
+      field.focus();
+    } else {
+      try {
+        const response = await profileService.updateLastName(field.value);
+        if (response.statusCode === 200) {
+          field.disabled = true;
+        }
+      } catch (e) {
+        showToastMessage('Failed to save the data. Please, try again.');
+      }
+    }
+  }
+
+  private async updateEmail(field: HTMLInputElement): Promise<void> {
+    if (field.disabled) {
+      field.disabled = false;
+      field.focus();
+    } else {
+      try {
+        const response = await profileService.updateEmail(field.value);
+        if (response.statusCode === 200) {
+          field.disabled = true;
+        }
+      } catch (e) {
+        showToastMessage('Failed to save the data. Please, try again.');
+      }
+    }
+  }
+
+  private async updateDateOfBirth(field: HTMLInputElement): Promise<void> {
+    if (field.disabled) {
+      field.disabled = false;
+      field.focus();
+    } else {
+      try {
+        const response = await profileService.updateDateOfBirth(field.value);
+        if (response.statusCode === 200) {
+          field.disabled = true;
+        }
+      } catch (e) {
+        showToastMessage('Failed to save the data. Please, try again.');
+      }
+    }
   }
 }
--- src/services/ProfileService.ts
+++ src/services/ProfileService.ts
@@ -8,6 +8,86 @@ export class ProfileService {
 
     return response;
   }
+
+  public async updateFirstName(firstName: string): Promise<ClientResponse<Customer>> {
+    const meData = await clientService.apiRoot.me().get().execute();
+    const { version } = meData.body;
+    const response = await clientService.apiRoot
+      .me()
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'setFirstName',
+              firstName,
+            },
+          ],
+        },
+      })
+      .execute();
+    return response;
+  }
+
+  public async updateLastName(lastName: string): Promise<ClientResponse<Customer>> {
+    const meData = await clientService.apiRoot.me().get().execute();
+    const { version } = meData.body;
+    const response = await clientService.apiRoot
+      .me()
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'setLastName',
+              lastName,
+            },
+          ],
+        },
+      })
+      .execute();
+    return response;
+  }
+
+  public async updateEmail(email: string): Promise<ClientResponse<Customer>> {
+    const meData = await clientService.apiRoot.me().get().execute();
+    const { version } = meData.body;
+    const response = await clientService.apiRoot
+      .me()
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'changeEmail',
+              email,
+            },
+          ],
+        },
+      })
+      .execute();
+    return response;
+  }
+
+  public async updateDateOfBirth(dateOfBirth: string): Promise<ClientResponse<Customer>> {
+    const meData = await clientService.apiRoot.me().get().execute();
+    const { version } = meData.body;
+    const response = await clientService.apiRoot
+      .me()
+      .post({
+        body: {
+          version,
+          actions: [
+            {
+              action: 'setDateOfBirth',
+              dateOfBirth,
+            },
+          ],
+        },
+      })
+      .execute();
+    return response;
+  }
 }
 
 const profileService = new ProfileService();
